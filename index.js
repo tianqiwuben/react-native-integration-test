eac4592f493b4166699dc83d8ddf56a0030ec16a
Tianqi Wu <tianqi@2redbeans.com>
2019-01-10 23:32:43 -0800

    fix extra import for app scope

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import TestHook from './src/TestHook';
 import TestOverlay from './src/TestOverlay';
 import Tester from './src/Tester';
-import Device from './src/Device';
 
 const IntegrationTest = {
   TestOverlay,
   Tester,
   refHook: TestHook.hook,
-  Device,
 };
 
 module.exports = IntegrationTest;
